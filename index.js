e2e4c17c01442aa270d2a1df49f991333e64f3a8
Karen Hanna <karen.hanna.802@gmail.com>
2025-05-21 09:08:40 -0400

    Adding routes for custom objects

--- index.js
+++ index.js
@@ -1,71 +1,56 @@
 const express = require('express');
 const axios = require('axios');
+const dotenv = require('dotenv');
 const app = express();
-
+dotenv.config();
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
-
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_TOKEN;
+const CUSTOM_OBJECT_NAME = process.env.CUSTOM_OBJECT_NAME;
+const headers = {
+  Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+  'Content-Type': 'application/json'
+};
+// ROUTE 1 - Homepage: list all plants
+app.get('/', async (req, res) => {
+  const url = `https://api.hubapi.com/crm/v3/objects/${CUSTOM_OBJECT_NAME}?properties=name,genus,species,light_requirement_s_,water_requirements`;
+  try {
+    const response = await axios.get(url, { headers });
+    const plants = response.data.results.filter(p => p.properties); // safety guard
+    console.log(`Fetched ${plants.length} plant(s)`); // lightweight log
+    res.render('homepage', { plants });
+  } catch (err) {
+    console.error('Error fetching plants:', err.response?.data || err.message);
+    res.status(500).send('Failed to fetch plants.');
+  }
 });
-
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
+// ROUTE 2 - Render form to add a new plant
+app.get('/update-cobj', (req, res) => {
+  res.render('updates', {
+    title: 'Update Custom Object Form | Integrating With HubSpot I Practicum'
+  });
 });
-*/
-
-
-// * Localhost
+// ROUTE 3 - Handle form submission
+app.post('/update-cobj', async (req, res) => {
+  const newPlant = {
+    properties: {
+      name: req.body.name,
+      genus: req.body.genus,
+      species: req.body.species,
+      water_requirements: req.body.water_requirements,
+      light_requirement_s_: req.body.light_requirement_s_ || ''
+    }
+  };
+  const url = `https://api.hubapi.com/crm/v3/objects/${CUSTOM_OBJECT_NAME}`;
+  try {
+    await axios.post(url, newPlant, { headers });
+    res.redirect('/');
+  } catch (err) {
+    console.error('Error creating plant:', err.response?.data || err.message);
+    res.status(500).send('Failed to create plant.');
+  }
+});
+// Start the server
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
